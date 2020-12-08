66d8d8f178d4fbfc09bdb228a92bd57f555a04bd
LAPTOP-R53CII4L\kirti <soni.kirtisoni@gmail.com>
2020-12-08 10:10:22 +0530

    commit for "passing parameters in the url and gets it in response".

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("check")
+var express = require('express');
+var app = express();
+app.get('/:name', function(req, res) {
+   res.send('Hello '+ req.params.name);
+});
+app.listen(4000);
